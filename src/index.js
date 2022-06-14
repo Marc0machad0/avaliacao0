50ea74749f1c3883266f9ba793687c57175df40b
paulo kafalque <Paulo-victorkafalque@hotmail.com>
2022-06-13 22:23:12 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const  express  =  require ( "express" ) ;
+const  app  =  express ( ) ;
+const  porta  =  processo . env . PORTO  ||  3000 ;
+
+aplicativo . get ( '/' ,  ( req ,  res )  =>  {
+    res . send ( "Atividade antes da avaliação!!" ) ;
+} )
+
+aplicativo . escutar ( porta ,  ( )  =>  {
+    consola . log ( "Servidor rodando na porta 3000" ) ;
+} )
